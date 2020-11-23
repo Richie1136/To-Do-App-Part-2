63a8557b6ba727bd62b1f2b894fc28fbbff39424
Richie1136 <Richiemajor1@gmail.com>
2020-11-23 10:17:00 -0500

    converted to functional components

--- src/App.js
+++ src/App.js
@@ -1,18 +1,17 @@
-import React, { Component } from "react";
-import {todos} from "./todos";
+import React, { useState } from "react";
+import {todos as todosList} from "./todos";
 
-class App extends Component {
-  state = {
-    todos: todos,
-  };
-  render() {
+function App() {
+ 
+const [todos, setTodos] = useState(todosList)
+
     return (
       <section className="todoapp">
         <header className="header">
           <h1>todos</h1>
           <input className="new-todo" placeholder="What needs to be done?" autofocus />
         </header>
-        <TodoList todos={Object.values(this.state.todos)} />
+        <TodoList todos={Object.values(todos)} />
         <footer className="footer">
           <span className="todo-count">
             <strong>0</strong> item(s) left
@@ -22,24 +21,22 @@ class App extends Component {
       </section>
     );
   }
-}
 
-class TodoItem extends Component {
-  render() {
+
+function TodoItem(props) {
+  
     return (
       <li className={this.props.completed ? "completed" : ""}>
         <div className="view">
-          <input className="toggle" type="checkbox" checked={this.props.completed} />
-          <label>{this.props.title}</label>
+          <input className="toggle" type="checkbox" checked={props.completed} />
+          <label>{props.title}</label>
           <button className="destroy" />
         </div>
       </li>
     );
   }
-}
 
-class TodoList extends Component {
-  render() {
+function TodoList(props) {
     return (
       <section className="main">
         <ul className="todo-list">
@@ -50,6 +47,5 @@ class TodoList extends Component {
       </section>
     );
   }
-}
 
 export default App;
